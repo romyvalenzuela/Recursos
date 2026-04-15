7e318598a3a8996a508f816c4d0e39d92f5015aa
Romy Valenzuela <romyvalenzuela311304@gmail.com>
2026-04-15 19:29:15 -0400

    Update and rename Practica to Practica_python.py

---
+++ Python/Practica_python.py
@@ -0,0 +1,32 @@
+Python
+print("HOLA MUNDO")
+a = 5
+b = 3
+print("suma:", a+b)
+
+a = 10
+b = 4 
+print("resta:", a-b)
+
+a = 6
+b = 2 
+print("Multiplicacion:", a*b)
+
+a = 20
+b = 5
+print("division:",a / b)
+
+edad = 18
+if edad >= 18:
+  print("mayor de edad")
+else:
+  print("meno de edad")
+for i in range(5):
+  print("for:",i)
+i = 1
+while i <= 5:
+  print("while:",i)
+  i +=1
+def saludo():
+  print("Hola Romy")
+  print
