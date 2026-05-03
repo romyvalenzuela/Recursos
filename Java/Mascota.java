aae1afbd40e31331988f6243968aecbf37b8a62c
Romy Valenzuela <romyvalenzuela311304@gmail.com>
2026-05-03 02:21:01 -0400

    Create Mascota class with attributes and constructor

---
+++ Java/Mascota.java
@@ -0,0 +1,10 @@
+public class Mascota {
+  private String nombre;
+  private int edad;
+  private int hambre = 50;
+  private int felicidad = 50;
+  private int vida = 100;
+
+  public Mascota(String nombre, int edad) {
+    this.nombre = nombre;
+    this
