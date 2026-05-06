ab1477f0cf13315180b3de016fffb24729ceda06
Romy Valenzuela <romyvalenzuela311304@gmail.com>
2026-05-06 10:07:44 -0400

    Update PersonajeHerencia.java

--- Java/PersonajeHerencia.java
+++ Java/PersonajeHerencia.java
@@ -10,7 +10,7 @@ public class PersonajeHerencia {
     arquero.atacar(maravilla);
     maravilla.atacar(arquero);
 
-    if (arquero.esta_vivo()) {
+    if (arquero.esta_Vivo()) {
         System.out.println(arquero.getNombre() + "ganó");
     } else {
         System.out.println(maravilla.getNombre() + "ganó");
@@ -20,7 +20,7 @@ public class PersonajeHerencia {
 
 }
 
-// Clase Padre 
+// Clase Base 
 class Personaje {
   private String nombre;
   private int vida;
@@ -30,40 +30,45 @@ class Personaje {
     this.nombre = nombre;
     this.vida = vida;
     this.ataque = ataque;
-}
 
-  public void atacar(Personaje otro) {
-    System.out.printLn(this.nombre + "ataca a " + otro.nombre);
-    otro.vida -= this.ataque;
-    System.out.printLn(otro.nombre + "queda con" + otro.vida + "de vida");
-}
+  }
 
-  public boolean esta_vivo() {
+  public void atacar(Personaje otroPersonaje) {
+    otroPersonaje.vida = otroPersonaje.vida - this.ataque;
+    System.out.println(this.nombre + "ataca a " + otroPersonaje.nombre);
+    System.out.println("Vida de " + otroPersonaje.nombre + ":" + otroPersonaje.vida);
+  }
+
+  public boolean estaVivo() {
     return this.vida > 0;
   }
 
-  public String getNombre() {
-    return this.nombre;
+  public String getNombre() { return nombre; }
+  public int getVida() { retutn vida; }
   }
 
-}
+//Clase hija - Herencia 
+class Guerrero extends Personaje { 
+  private int defensa;
 
-//Clase Hija con Herencia 
-class Guerrero extends Personaje {
-  private int armadura;
-
-  public Guerrero(String nombre, int vida, int ataque, int armadura) {
-    super(nombre, vida, ataque); 
-    this.armadura = armadura;
+  public Guerrero(String nombre, int vida, int ataque, int defensa) {
   }
 
   public void defender() {
-    System.out.printLn(getNombre() + " se defiende y gana " + this.armadura + " vida ");
+    System.out.prinln(this.nombre + " se defiende. Defensa: " + this.defensa);
+  }
 
+  public void atacar(Personaje otroPersonaje) {
+    int danio = this.ataque; //Podria restar defensa del otro si quiere
+    otroPersonaje.vida = otroPersonaje.vida - danio;
+    System.out.println(this.nombre + " ataca en furia a " + otroPersonaje.nombre);
+    System.out.println(" Vida de " + otroPersonaje + ":" + otroPersonaje.getvida());
   }
 }
 
-}
+  
+ 
+
 
 
   
