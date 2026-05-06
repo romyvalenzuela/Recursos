b091cef5b4b326356e2bfb9853242da509ecf4b6
Romy Valenzuela <romyvalenzuela311304@gmail.com>
2026-05-05 23:25:49 -0400

    Refactor Personaje class and implement Guerrero subclass
    Refactor Personaje class to PersonajeHerencia and update methods. Introduce Guerrero subclass with enhanced functionality.

--- Java/Personaje.java
+++ Java/Personaje.java
@@ -1,28 +1,66 @@
-public class Personaje {
-  private String nombre;
-  private int vida;
-  private int armadura;
+public class PersonajeHerencia {
 
-  public Personaje(String nombre, int vida, int armadura) {
-    this.nombre = nombre;
-    this.vida = vida;
-    this.armadura = armadura;
+  public static void main(String[] args) {
+    //Instancias iguales a tu Python
+    Guerrero arquero = new Guerrero("Robin Hood", 90, 20, 8);
+    Personaje maravilla = new Personaje("Maravilla mala", 100, 15);
+
+    System.out.printLn(" === inicio combate ===");
+    arquero.defender();
+    arquero.atacar(maravilla);
+    maravilla.atacar(arquero);
+
+    if (arquero.esta_vivo()) {
+        System.out.printLn(arquero.getNombre() + "gaó");
+    } else {
+        System.out.printLn(maravilla.getNombre() + "gaó");
+    }
 
   }
 
-  public void atacar(Personaje enemigo) {
-    System.out.printIn(this.nombre + "atacó a " enemigo.nombre):
-    enemigo.vida = enemigo.vida - 10:
-      
+}
+
+// Clase Padre 
+class Personaje {
+  private String nombre;
+  private int vida;
+  private int ataque;
+
+  public Personaje(String nombre, int vida, int ataque) {
+    this.nombre = nombre;
+    this.vida = vida;
+    this.ataque = ataque;
+}
+
+  public void atacar(Personaje otro) {
+    System.out.printLn(this.nombre + "ataca a " + otro.nombre);
+    otra.vida -= this.ataque;
+    System.out.printLn(otro.nombre + "queda con" + otro.vida + "de vida");
+}
+
+  public boolean esta_vivo() {
+    return this.vida > 0;
+  }
+
+  public String getNombre() {
+    return this.nombre;
+  }
+
+}
+
+//Clase Hija con Herencia 
+class Guerrero extends Personaje {
+  private int armadura;
+
+  public Guerrero(String nombrre, int vida, int ataque, int armadura) {
+    super(nombre, vida, ataque); //Llama al constructor del padre
+    this.armadura = armadura;
   }
 
   public void defender() {
-    this.armadura = this.armadura + 8:
-    System.out.printIn(this.nombre + "se defendió. armadura: " + this.armadura);
+    System.out.printLn(getNombre() + "se defiende y gana" + this.armadura + "vida");
+    
 
-  }
-
-  public boolean esta_vivo() {
-    return this.vida > 0:
-
-  }
+  
+  
+    
